9fac03a504cc295349deb9169609ed7f305db223
CarterDesigner <bbjcarter12@gmail.com>
2024-02-05 01:06:53 +0000

    cta-1 section half-completion
    made it responsive
    
    need to amend 3d model sizing issue

--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,16 @@ import { Application } from '@splinetool/runtime';
 
 const canvas = document.getElementById('canvas3d');
 const app = new Application(canvas);
+
+function resizeCanvas() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  canvas.width = width;
+  canvas.height = height;
+  app.camera.aspect = width / height;
+  app.camera.updateProjectionMatrix();
+  app.render();
+}
 app.load('https://prod.spline.design/VAZOR9VRZAigE7Uz/scene.splinecode');
+window.addEventListener('resize', resizeCanvas);
+resizeCanvas();
