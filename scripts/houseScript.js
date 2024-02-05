f6e94be298215d68f4f1ae6b213b85c72f4662be
CarterDesigner <bbjcarter12@gmail.com>
2024-02-05 16:38:04 +0000

    3d-model update

--- scripts/houseScript.js
+++ scripts/houseScript.js
@@ -22,3 +22,51 @@ function outDropMenuClick(event) {
     }
 }
 document.addEventListener('click', outDropMenuClick);
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let interval = null;
+let hasPlayedAutomatically = false;
+
+const targetSpan = document.querySelector(".cta-1 .cta1-container .cta-text .number-line span");
+
+// Function to check if the element is 80vh from the top
+function isElement80vhFromTop(element) {
+    const elementTop = element.getBoundingClientRect().top;
+    return elementTop <= window.innerHeight * 0.8;
+}
+
+function startAnimation() {
+    let iteration = 0;
+    clearInterval(interval);
+
+    interval = setInterval(() => {
+        targetSpan.innerText = targetSpan.innerText
+            .split("")
+            .map((letter, index) => {
+                if (index < iteration) {
+                    return targetSpan.dataset.value[index];
+                }
+                return letters[Math.floor(Math.random() * 26)];
+            })
+            .join("");
+
+        if (iteration >= targetSpan.dataset.value.length) {
+            clearInterval(interval);
+        }
+        iteration += 1 / 4;
+    }, 40);
+}
+
+// Check for the element position on scroll
+window.addEventListener('scroll', () => {
+    if (!hasPlayedAutomatically && isElement80vhFromTop(targetSpan)) {
+        // Start the animation automatically once
+        startAnimation();
+        hasPlayedAutomatically = true;
+    }
+});
+
+// Start the animation on mouseover
+targetSpan.onmouseover = () => {
+    startAnimation();
+};
--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,13 @@ import { Application } from '@splinetool/runtime';
 const canvas = document.getElementById('canvas3d');
 const app = new Application(canvas);
 app.load('https://prod.spline.design/VAZOR9VRZAigE7Uz/scene.splinecode');
+
+function onWindowResize() {
+    const newWidth = window.innerWidth;
+    const newHeight = window.innerHeight;
+
+    canvas.width = newWidth;
+    canvas.height = newHeight;
+}
+window.addEventListener('resize', onWindowResize);
+onWindowResize();
